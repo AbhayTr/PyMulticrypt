d26a11ed84d2e2d78ea1d130c90c6b47f4e70850
AbhayTr <53339132+AbhayTr@users.noreply.github.com>
2020-11-10 14:49:08 +0530

    Add files via upload

--- end2end.py
+++ end2end.py
@@ -1,70 +1,104 @@
-import hashlib
 import random
-from datetime import datetime
+
+primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349]
+
+def n_bits_prime(n):
+    while True:
+        prime_number = random.randrange((2 ** (n-1)) + 1, (2 ** n) - 1)
+        for divisor in primes_list:
+            if prime_number % divisor == 0 and divisor ** 2 <= prime_number:
+                break
+        else:
+            return prime_number
+  
+def check_prime_strength(prime_number): 
+    max_divisions_two = 0
+    ec = prime_number - 1
+    while ec % 2 == 0:
+        ec >>= 1
+        max_divisions_two += 1
+    assert 2 ** max_divisions_two * ec == prime_number - 1
+  
+    def trial_composite(round_tester):
+        if pow(round_tester, ec, prime_number) == 1: 
+            return False
+        for i in range(max_divisions_two): 
+            if pow(round_tester, 2 ** i * ec, prime_number) == prime_number - 1: 
+                return False
+        return True
+  
+    for i in range(20): 
+        round_tester = random.randrange(2, prime_number) 
+        if trial_composite(round_tester):
+            return False
+    return True
 
 def is_coprime(number1, number2):
     while number2 != 0:
         number1, number2 = number2, number1 % number2
     return number1 == 1
 
-def get_keys(unique_id = ""):
-    number_private_factors = 21
-    private_key = str(random.randint(0,500000) + datetime.timestamp(datetime.now())) + unique_id
-    public_key = 1
-    public_key_prime = 1
-    for encrypting in range(number_private_factors):
-        sha256_private_key = hashlib.sha256(private_key.encode()).hexdigest()
-        current_key = ""
-        for charecter in sha256_private_key:
-            current_key += str(ord(charecter)) + str(random.randint(0, 9))
-        if public_key == 1:
-            private_key = current_key
-        else:
-            private_key += "X" + current_key
-        current_key = int(current_key)
-        public_key = public_key * current_key
-        public_key_prime = public_key_prime * (current_key - 1)
-    public_key_relative_prime = 0
-    for number in range(2, public_key_prime + 1):
-        if is_coprime(public_key_prime, number):
-            public_key_relative_prime = number
+def mod_inverse(number1, number2):
+
+    def eea(number1, number2):
+        if number2 == 0:
+            return (1,0)
+        (q,r) = (number1 // number2, number1 % number2)
+        (s,t) = eea(number2, r)
+        return (t, s - (q * t))
+
+    inv = eea(number1, number2)[0]
+    if inv < 1:
+        inv += number2
+    return inv
+
+def get_keys():
+    n = 1024
+    prime_key_1 = 0
+    prime_key_2 = 0
+    while True:
+        prime_key = n_bits_prime(n)
+        if check_prime_strength(prime_key):
+            if prime_key_1 == 0:
+                prime_key_1 = prime_key
+                continue
+            elif prime_key_2 == 0:
+                prime_key_2 = prime_key
+                break
+    public_key_number = prime_key_1 * prime_key_2
+    phi_n = (prime_key_1 - 1) * (prime_key_2 - 1)
+    e = 0
+    for number in range(2, phi_n):
+        if is_coprime(phi_n, number):
+            e = number
             break
-    public_key = str(public_key) + "X" + str(public_key_relative_prime)
-    private_key += "X" + str(public_key_prime)
+    public_key = str(public_key_number) + "X" + str(e)
+    private_key = str(mod_inverse(e, phi_n))
     return {"private": private_key, "public": public_key}
 
-def encrypt(public_key, message):
+def encrypt(message, public_key):
     seperator_position = public_key.index("X")
-    relative_prime = int(public_key[seperator_position + 1:])
-    public_key = int(public_key[:seperator_position])
+    e = int(public_key[seperator_position + 1:])
+    public_key_number = int(public_key[:seperator_position])
     encrypted_message = ""
     charecters = list(message)
     for charecter_index in range(len(charecters)):
-        encrypted_charecter = public_key % (ord(charecters[charecter_index]) ** relative_prime)
-        encrypted_message += str(encrypted_charecter)
+        encrypted_message += str(ord(charecters[charecter_index]))
         if charecter_index != len(charecters) - 1:
-            encrypted_message += "X"
+            encrypted_message += "300"
+    encrypted_message = str(int((int(encrypted_message) ** e) % public_key_number))        
     return encrypted_message
 
-def decrypt(private_key, public_key, message):
-    private_factors = private_key.replace("X", " ").split(" ")
-    decrypt_factor = int(private_factors[len(private_factors) - 1])
-    charecters = message.replace("X", " ").split(" ")
+def decrypt(message, private_key, public_key): 
     seperator_position = public_key.index("X")
-    relative_prime = int(public_key[seperator_position + 1:])
-    public_key = int(public_key[:seperator_position])
-    decrypt_power = 0
-    for number in range(1, decrypt_factor + 1):
-        if (relative_prime * number) % decrypt_factor == 1:
-            decrypt_power = number
-            break
+    public_key_number = int(public_key[:seperator_position])
+    charecters = str(pow(int(message), int(private_key), public_key_number)).replace("300", " ").split(" ")
     actual_message = ""
     for charecter in charecters:
-        actual_charecter = chr((int(charecter) ** decrypt_power) % public_key)
-        actual_message += actual_charecter
+        actual_message += chr(int(charecter))
     return actual_message
 
-keys = get_keys(input("Enter Username: "))
+keys = get_keys()
 print()
 print("Private Key:")
 print()
@@ -76,14 +110,12 @@ print(keys["public"])
 print()
 message = input("Enter Message: ")
 print()
-print("Actual Message: " + message)
-print()
 print("Encrypted Message:")
 print()
-msg = encrypt(keys["public"], message)
+msg = encrypt(message, keys["public"])
 print(msg)
 print()
 print("Decrypted Message:")
 print()
-print(decrypt(keys["private"], keys["public"], msg))
+print(decrypt(msg, keys["private"], keys["public"]))
 print()
